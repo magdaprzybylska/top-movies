ff0607edeb114998ba7e1e1189885d71405e32bf
Magda Przybylska <magda.przybylska0811@gmail.com>
2024-01-04 11:18:53 +0100

    separated db updating from api calling

--- app/config.py
+++ app/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, ".env"))
-api = {"api": {"api_token": os.getenv("ACCESS_TOKEN_AUTH")}}
 
 class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY")
