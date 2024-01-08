fb6e38d7a8d29b0b1282a5dd808a329e7d54aa27
Noemi Kowalewska <noemikow8@gmail.com>
2024-01-08 18:02:52 +0100

    use magic mock

--- tests/unit/test_api_caller.py
+++ tests/unit/test_api_caller.py
@@ -1,6 +1,6 @@
 import datetime
 import os
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, patch
 import pytest
 
 from app.main.api_caller import MovieApiCaller, MOVIE_DB_IMAGE_URL
@@ -20,7 +20,7 @@ def test_get_movie(api_caller):
         "overview": "This is a movie overview",
         "poster_path": "/example_path.jpg",
     }
-    mocked_response = Mock()
+    mocked_response = MagicMock()
     mocked_response.json.return_value = response_data
     # when
     with patch("requests.get", return_value=mocked_response):
@@ -39,9 +39,10 @@ def test_get_movie(api_caller):
 def test_get_movies_to_select_success(api_caller):
     # given
     response_data = {"results": [{"title": "Movie 1"}, {"title": "Movie 2"}]}
-    mocked_response = Mock()
+    mocked_response = MagicMock()
     mocked_response.status_code = 200
     mocked_response.json.return_value = response_data
+
     # when
     with patch("requests.get", return_value=mocked_response):
         movies = api_caller.get_movies_to_select("query")
@@ -52,7 +53,7 @@ def test_get_movies_to_select_success(api_caller):
 
 
 def test_get_movies_to_select_failure(api_caller):
-    mocked_response = Mock()
+    mocked_response = MagicMock()
     mocked_response.status_code = 500
 
     with patch("requests.get", return_value=mocked_response):
